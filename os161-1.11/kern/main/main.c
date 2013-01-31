a4c1ae622747c78b75ed699d0227707516a71b1c
skoh-fley <qhiiyr@gmail.com>
2013-01-31 14:43:56 -0800

    Implemented _exit syscall

--- os161-1.11/kern/main/main.c
+++ os161-1.11/kern/main/main.c
@@ -151,6 +151,18 @@ int _printint(int value) {
 }
 
 int _printstring(char *string, int numchars) {
+  /* Copy the string from the user address into a kernel buffer, and then print
+   * it using kprintf. Be careful to check for misuse by the user (for example,
+   * the memory pointed to by "string" may not contain a proper null-terminated
+   * string of length numchars). If an error is detected, errno should be set
+   * and an appropriate result returned. Otherwise, the return value should be
+   * the return value from kprintf.
+   */
+}
+
+void _exit(int code) {
+  /* Only call thread_exit from kernel space. */
+  thread_exit();
 }
 
 /*
