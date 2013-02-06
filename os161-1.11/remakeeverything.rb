5491b65e6694854d355cd23f681310c84e5af475
Chris Hansen <moosekid5@gmail.com>
2013-02-05 16:37:58 -0800

    Added ruby script to remake kernel, userlevel code and main executable

---
+++ os161-1.11/remakeeverything.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+DEBUG = ARGV
+debugflag = "-v"
+if  DEBUG && (DEBUG[0].eql? debugflag)
+  code = `cd kern/conf; ./config ASST0 > /dev/null`
+  puts "Just ran config"
+  code = `cd kern/compile/ASST0; make depend && make && make install > /dev/null`
+  puts "Just ran make depend, make and make install"
+  code = `cd testbin; make && make install > /dev/null`
+  puts "Made userlevel code"
+  puts "Making main executable"
+  code = `make > /dev/null`
+  puts "Finished making system"
+  puts "All done!"
+else
+  code = `cd kern/conf; ./config ASST0 > /dev/null 2> /dev/null`
+  puts "Just ran config"
+  code = `cd kern/compile/ASST0; make depend && make && make install > /dev/null 2> /dev/null`
+  puts "Just ran make depend, make and make install"
+  code = `cd testbin; make && make install > /dev/null 2> /dev/null`
+  puts "Made userlevel code"
+  puts "Making main executable"
+  code = `make > /dev/null 2> /dev/null`
+  puts "Finished making system"
+  puts "All done!"
+end
