7354cb4044a3326f75cfe2de92c2a709f311af14
Chris Hansen <cthansen22@gmail.com>
2013-03-08 12:16:13 -0800

    Add almost all stoplight code
    The stoplight works, the print isn't being done correctly

--- os161-1.11/kern/asst1/stoplight.c
+++ os161-1.11/kern/asst1/stoplight.c
@@ -18,6 +18,7 @@
 #include <lib.h>
 #include <test.h>
 #include <thread.h>
+#include <synch.h>
 
 
 /*
@@ -32,6 +33,34 @@
 
 #define NCARS 20
 
+/*
+ * Directions
+ */ 
+
+#define NORTH 0
+#define EAST 1
+#define SOUTH 2
+#define WEST 3
+#define RIGHT_TURN 4
+#define STRAIGHT 5
+#define LEFT_TURN 6
+
+/*
+ * Global variables
+ */
+
+struct lock *leftturnlock;
+struct lock *nApproach;
+struct lock *eApproach;
+struct lock *sApproach;
+struct lock *wApproach;
+struct lock *segments[4];
+struct lock *carvar;
+struct cv *nWait;
+struct cv *eWait;
+struct cv *sWait;
+struct cv *wWait;
+int numcarsleft;
 
 /*
  *
@@ -59,15 +88,56 @@
 
 static
 void
-gostraight(unsigned long cardirection,
-           unsigned long carnumber)
+gostraight(unsigned long cardirection, unsigned long carnumber)
 {
-        /*
-         * Avoid unused variable warnings.
-         */
-        
-        (void) cardirection;
-        (void) carnumber;
+  /*
+   * Avoid unused variable warnings.
+   */
+
+  (void) cardirection;
+  (void) carnumber;
+
+  switch (cardirection) {
+    case NORTH:
+      lock_acquire(segments[0]);
+      lock_acquire(segments[2]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(nApproach);
+      lock_release(segments[0]);
+      lock_release(segments[2]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case EAST:
+      lock_acquire(segments[1]);
+      lock_acquire(segments[0]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(eApproach);
+      lock_release(segments[1]);
+      lock_release(segments[0]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case WEST:
+      lock_acquire(segments[2]);
+      lock_acquire(segments[3]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(wApproach);
+      lock_release(segments[2]);
+      lock_release(segments[3]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case SOUTH:
+      lock_acquire(segments[3]);
+      lock_acquire(segments[1]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(sApproach);
+      lock_release(segments[3]);
+      lock_release(segments[1]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+  }
+  lock_acquire(carvar);
+  numcarsleft = numcarsleft - 1;
+  lock_release(carvar);
 }
 
 
@@ -90,15 +160,72 @@ gostraight(unsigned long cardirection,
 
 static
 void
-turnleft(unsigned long cardirection,
-         unsigned long carnumber)
+turnleft(unsigned long cardirection, unsigned long carnumber)
 {
-        /*
-         * Avoid unused variable warnings.
-         */
+  /*
+   * Avoid unused variable warnings.
+   */
 
-        (void) cardirection;
-        (void) carnumber;
+  (void) cardirection;
+  (void) carnumber;
+
+  switch (cardirection) {
+    case NORTH:
+      lock_acquire(leftturnlock);
+      lock_acquire(segments[0]);
+      lock_acquire(segments[2]);
+      lock_acquire(segments[3]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(nApproach);
+      lock_release(segments[0]);
+      lock_release(segments[2]);
+      lock_release(segments[3]);
+      lock_release(leftturnlock);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case EAST:
+      lock_acquire(leftturnlock);
+      lock_acquire(segments[1]);
+      lock_acquire(segments[0]);
+      lock_acquire(segments[2]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(eApproach);
+      lock_release(segments[1]);
+      lock_release(segments[0]);
+      lock_release(segments[2]);
+      lock_release(leftturnlock);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case WEST:
+      lock_acquire(leftturnlock);
+      lock_acquire(segments[2]);
+      lock_acquire(segments[3]);
+      lock_acquire(segments[1]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(wApproach);
+      lock_release(segments[2]);
+      lock_release(segments[3]);
+      lock_release(segments[1]);
+      lock_release(leftturnlock);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case SOUTH:
+      lock_acquire(leftturnlock);
+      lock_acquire(segments[3]);
+      lock_acquire(segments[1]);
+      lock_acquire(segments[0]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(sApproach);
+      lock_release(segments[3]);
+      lock_release(segments[1]);
+      lock_release(segments[0]);
+      lock_release(leftturnlock);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+  }
+  lock_acquire(carvar);
+  numcarsleft = numcarsleft - 1;
+  lock_release(carvar);
 }
 
 
@@ -121,15 +248,48 @@ turnleft(unsigned long cardirection,
 
 static
 void
-turnright(unsigned long cardirection,
-          unsigned long carnumber)
+turnright(unsigned long cardirection, unsigned long carnumber)
 {
-        /*
-         * Avoid unused variable warnings.
-         */
+  /*
+   * Avoid unused variable warnings.
+   */
 
-        (void) cardirection;
-        (void) carnumber;
+  (void) cardirection;
+  (void) carnumber;
+
+  switch (cardirection) {
+    case NORTH:
+      lock_acquire(segments[0]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(nApproach);
+      lock_release(segments[0]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case EAST:
+      lock_acquire(segments[1]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(eApproach);
+      lock_release(segments[1]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case WEST:
+      lock_acquire(segments[2]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(wApproach);
+      lock_release(segments[2]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+    case SOUTH:
+      lock_acquire(segments[3]);
+      kprintf("Car %lu is entering the intersection\n", carnumber);
+      lock_release(sApproach);
+      lock_release(segments[3]);
+      kprintf("Car %lu is leaving the intersection\n", carnumber);
+      break;
+  }
+  lock_acquire(carvar);
+  numcarsleft = numcarsleft - 1;
+  lock_release(carvar);
 }
 
 
@@ -152,31 +312,66 @@ turnright(unsigned long cardirection,
  *      or going straight should be done by calling one of the functions
  *      above.
  */
- 
+
 static
 void
-approachintersection(void * unusedpointer,
-                     unsigned long carnumber)
+approachintersection(void * unusedpointer, unsigned long carnumber)
 {
-        int cardirection;
+  (void) unusedpointer;
+  int cardirection;
+  int turnChoice;
 
-        /*
-         * Avoid unused variable and function warnings.
-         */
+  turnChoice = (random() % 3)+4;
+  cardirection = random() % 4; 
 
-        (void) unusedpointer;
-        (void) carnumber;
-	(void) gostraight;
-	(void) turnleft;
-	(void) turnright;
 
-        /*
-         * cardirection is set randomly.
-         */
+  kprintf("Car %lu is approaching the intersection\n", carnumber);
 
-        cardirection = random() % 4;
+  switch (cardirection) {
+    case NORTH:
+      lock_acquire(nApproach);
+      cv_wait(nWait, nApproach);
+      break;
+    case EAST:
+      lock_acquire(eApproach);
+      cv_wait(eWait, eApproach);
+      break;
+    case SOUTH:
+      lock_acquire(sApproach);
+      cv_wait(sWait, sApproach);
+      break;
+    case WEST:
+      lock_acquire(wApproach);
+      cv_wait(wWait, wApproach);
+      break;
+  }
+
+  switch (turnChoice) {
+    case RIGHT_TURN:
+      turnright(cardirection, carnumber);
+      break;
+    case LEFT_TURN:
+      turnleft(cardirection, carnumber);
+      break;
+    case STRAIGHT:
+      gostraight(cardirection, carnumber);
+      break;
+  }
 }
 
+void stoplight() {
+  int i;
+  while (numcarsleft) {
+    for (i = 0; i < NCARS; ++i) {
+      cv_signal(nWait, nApproach);
+      cv_signal(sWait, sApproach);
+    }
+    for (i = 0; i < NCARS; ++i) {
+      cv_signal(eWait, eApproach);
+      cv_signal(wWait, wApproach);
+    }
+  }
+}
 
 /*
  * createcars()
@@ -194,42 +389,64 @@ approachintersection(void * unusedpointer,
  */
 
 int
-createcars(int nargs,
-           char ** args)
+createcars(int nargs, char ** args)
 {
-        int index, error;
+  int index, error;
 
-        /*
-         * Avoid unused variable warnings.
-         */
+  leftturnlock = lock_create("leftturnlock");
+  nApproach = lock_create("nApproach");
+  eApproach = lock_create("eApproach");
+  sApproach = lock_create("sApproach");
+  wApproach = lock_create("wApproach");
+  carvar = lock_create("carvar");
+  nWait = cv_create("nWait");
+  eWait = cv_create("eWait");
+  sWait = cv_create("sWait");
+  wWait = cv_create("wWait");
+  segments[0] = lock_create("nw");
+  segments[1] = lock_create("ne");
+  segments[2] = lock_create("sw");
+  segments[3] = lock_create("se");
 
-        (void) nargs;
-        (void) args;
+  numcarsleft = 20;
 
-        /*
-         * Start NCARS approachintersection() threads.
-         */
+  /*
+   * Avoid unused variable warnings.
+   */
 
-        for (index = 0; index < NCARS; index++) {
+  (void) nargs;
+  (void) args;
 
-                error = thread_fork("approachintersection thread",
-                                    NULL,
-                                    index,
-                                    approachintersection,
-                                    NULL
-                                    );
+  /*
+   * Start NCARS approachintersection() threads.
+   */
 
-                /*
-                 * panic() on error.
-                 */
+  for (index = 0; index < NCARS; index++) {
 
-                if (error) {
-                        
-                        panic("approachintersection: thread_fork failed: %s\n",
-                              strerror(error)
-                              );
-                }
-        }
+    error = thread_fork("approachintersection thread",
+        NULL,
+        index,
+        approachintersection,
+        NULL
+        );
 
-        return 0;
+    /*
+     * panic() on error.
+     */
+
+    if (error) {
+
+      panic("approachintersection: thread_fork failed: %s\n",
+          strerror(error)
+          );
+    }
+  }
+
+  /*
+   * Start stoplight
+   */
+
+  stoplight();
+
+  return 0;
 }
