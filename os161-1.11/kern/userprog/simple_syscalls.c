5134eb9cd0eabe0a57e5fd088957e597158dcf41
skoh-fley <qhiiyr@gmail.com>
2013-04-05 14:56:05 -0700

    added empty syscalls

--- os161-1.11/kern/userprog/simple_syscalls.c
+++ os161-1.11/kern/userprog/simple_syscalls.c
@@ -1,6 +1,8 @@
 #include <types.h>
 #include <syscall.h>
 
+#include <kern/hash.h>
+
 int _helloworld() {
   return kprintf("Hello World\n");
 }
@@ -18,6 +20,26 @@ int _printstring(char *string, int numchars) {
   }
   int errno = -1; // Todo: correct this errno
   return errno;
+}
+
+int getpid() {
+  // Todo
+}
+
+char readchar(void) {
+  // Todo
+}
+
+void fork() {
+  // Todo
+}
+
+void execv() {
+  // Todo -- the heart of the assignment
+}
+
+waitpid() {
+  // Todo
 }
 
 void _exit(int code) {
